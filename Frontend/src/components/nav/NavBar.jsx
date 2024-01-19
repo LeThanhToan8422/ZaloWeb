a70bf8a0ef035e5edc1b415fc05ac6c696e77fbd
iamngo <88821216+iamngo@users.noreply.github.com>
2024-01-19 17:59:37 +0700

    finish slide

--- Frontend/src/components/nav/NavBar.jsx
+++ Frontend/src/components/nav/NavBar.jsx
@@ -3,7 +3,7 @@ import "../../sass/NavBar.scss"
 const NavBar = () => {
   return (
     <div className='container-nav-bar'>
-      <div className='nav-avt'></div>
+      <div className='nav-avt'><i className="fa-solid fa-user-tie icon"></i></div>
       <div className='nav-group'>
         <div className='nav-group-chat'>
           <div className='group-icon'><i className="fa-regular fa-comment-dots icon"></i></div>

266c35444b94467b0f6e688d2b0cead50bc67d48
iamngo <88821216+iamngo@users.noreply.github.com>
2024-01-19 16:43:13 +0700

    finish navbar

--- Frontend/src/components/contentChat/ContentChat.jsx
+++ Frontend/src/components/contentChat/ContentChat.jsx
@@ -1,8 +1,31 @@
-import "../../sass/ContentChat.scss"
+
+import "../../sass/ContentChat.scss";
+// import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Navigation } from 'swiper/modules';
 
 const ContentChat = () => {
   return (
-    <div className="container-content-chat">ContentChat</div>
+    <div className="container-content-chat">
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide><img className="slide" alt="" src="/slide1.png"/></SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+  </div>
   )
 }
 
--- Frontend/src/components/nav/NavBar.jsx
+++ Frontend/src/components/nav/NavBar.jsx
@@ -2,7 +2,21 @@ import "../../sass/NavBar.scss"
 
 const NavBar = () => {
   return (
-    <div className='container-nav-bar'>NavBar</div>
+    <div className='container-nav-bar'>
+      <div className='nav-avt'></div>
+      <div className='nav-group'>
+        <div className='nav-group-chat'>
+          <div className='group-icon'><i className="fa-regular fa-comment-dots icon"></i></div>
+          <div className='group-icon'><i className="fa-regular fa-address-book icon"></i></div>
+          <div className='group-icon'><i className="fa-regular fa-square-check icon"></i></div>
+        </div>
+        <div className='nav-group-utilities'>
+          <div className='group-icon'><i className="fa-solid fa-cloud icon"></i></div>
+          <div className="group-icon"><i className="fa-solid fa-toolbox icon"></i></div>
+          <div className='group-icon'><i className="fa-solid fa-gear icon"></i></div>
+        </div>
+      </div>
+    </div>
   )
 }
 

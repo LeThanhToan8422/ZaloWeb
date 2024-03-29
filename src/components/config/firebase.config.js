b5b058b6a6539db917dd3d5d28b7412f995654e9
Lê Thanh Toàn <toan20040331@gmail.com>
2024-03-29 22:19:51 +0700

    finish UI send and verify otp

--- src/components/config/firebase.config.js
+++ src/components/config/firebase.config.js
@@ -6,7 +6,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: "AIzaSyDU9bDBBwg8kfJeSAx5YXFRxCQaC9N1frE",
   authDomain: "otp-zalo-d6833.firebaseapp.com",
   projectId: "otp-zalo-d6833",
@@ -14,6 +14,15 @@ const firebaseConfig = {
   messagingSenderId: "32668031824",
   appId: "1:32668031824:web:3e90f2d08d08115c32ea55",
   measurementId: "G-6DJSHK3X4R"
+};*/
+const firebaseConfig = {
+  apiKey: "AIzaSyAcl6RurKXuDCpv9hJd3_MTEp9ur3WRuX0",
+  authDomain: "otp-zalo-web.firebaseapp.com",
+  projectId: "otp-zalo-web",
+  storageBucket: "otp-zalo-web.appspot.com",
+  messagingSenderId: "246626453583",
+  appId: "1:246626453583:web:71c929c4cf204c5baac23a",
+  measurementId: "G-EBR6M1KL1X"
 };
 
 // Initialize Firebase

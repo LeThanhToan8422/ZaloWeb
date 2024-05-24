becf770df5c3d792c8fc41d7a6b9aba250009ee2
Lê Thanh Toàn <toan20040331@gmail.com>
2024-05-24 14:29:51 +0700

    UI wait message

--- src/components/listChat/ListChat.jsx
+++ src/components/listChat/ListChat.jsx
@@ -5,6 +5,7 @@ import "moment/locale/vi";
 import FormSearchFriendByPhone from "./components/FormSearchFriendByPhone";
 import FormDeleteChat from "./components/FormDeleteChat";
 import FormCreateGroup from "./components/FormCreateGroup";
+import axios from "axios";
 
 const ListChat = ({
   displayListChat,
@@ -17,7 +18,7 @@ const ListChat = ({
   setRerender,
   urlBackend,
   makeFriends,
-  setDeleteChat
+  setDeleteChat,
 }) => {
   const [isPrioritize, setIsPrioritize] = useState(true);
   const [chatSelected, setChatsSelected] = useState(0);
@@ -28,7 +29,7 @@ const ListChat = ({
     "https://s3-dynamodb-cloudfront-20040331.s3.ap-southeast-1.amazonaws.com/"
   );
   const [isClickUtils, setIsClickUtils] = useState(false);
-  const [idChat, setIdChat] = useState('');
+  const [idChat, setIdChat] = useState("");
   const [visibleDel, setVisibleDel] = useState(false);
   const [visibleGroup, setVisibleGroup] = useState(false);
 
@@ -55,25 +56,26 @@ const ListChat = ({
     }
   }, [JSON.stringify(messageFinal)]);
 
-  let handleClickChat = (chat) => {
-    if(chat.phone){
+  let handleClickChat = async(chat) => {
+    if (chat.phone) {
+      await axios.post(`${urlBackend}/wait-message/update/${chat.id}/${userId}`)
       handleChangeChat({
-        id : chat.id,
-        type : "Single"
+        id: chat.id,
+        type: "Single",
       });
       setChatsSelected({
-        id : chat.id,
-        type : "Single"
+        id: chat.id,
+        type: "Single",
       });
-    }
-    else{
+    } else {
+      await axios.post(`${urlBackend}/wait-message/update/${userId}/Group/${chat.id}`)
       handleChangeChat({
-        id : chat.id,
-        type : "Group"
+        id: chat.id,
+        type: "Group",
       });
       setChatsSelected({
-        id : chat.id,
-        type : "Group"
+        id: chat.id,
+        type: "Group",
       });
     }
   };
@@ -86,10 +88,12 @@ const ListChat = ({
   let handleClickUtils = (id) => {
     setIsClickUtils(!isClickUtils);
     setIdChat(id);
-
-  }
+  };
   return (
-    <div className="container-list-chat" style={{display : displayListChat ? "flex" : "none"}}>
+    <div
+      className="container-list-chat"
+      style={{ display: displayListChat ? "flex" : "none" }}
+    >
       <div className="contents-search-types">
         <div className="content-search">
           <div className="search">
@@ -114,18 +118,28 @@ const ListChat = ({
             className="fa-solid fa-user-plus icon-user"
             onClick={() => setVisibleFriendByPhone(true)}
           ></i>
-          <span style={{color: 'red', marginBottom:20,marginLeft:-10, fontSize:12}}>{makeFriends?.length>0?`+` + makeFriends?.length:""}</span>
-          <i className="fa-solid fa-users icon-user" onClick={() => (setVisibleGroup(true))}></i>
-          {
-            visibleGroup 
-            &&
-            <FormCreateGroup 
-            visible={visibleGroup} 
-            setVisible={setVisibleGroup}
-            userId={userId}
-            urlBackend={urlBackend}
+          <span
+            style={{
+              color: "red",
+              marginBottom: 20,
+              marginLeft: -10,
+              fontSize: 12,
+            }}
+          >
+            {makeFriends?.length > 0 ? `+` + makeFriends?.length : ""}
+          </span>
+          <i
+            className="fa-solid fa-users icon-user"
+            onClick={() => setVisibleGroup(true)}
+          ></i>
+          {visibleGroup && (
+            <FormCreateGroup
+              visible={visibleGroup}
+              setVisible={setVisibleGroup}
+              userId={userId}
+              urlBackend={urlBackend}
             />
-          }
+          )}
         </div>
 
         <div className="content-type">
@@ -166,10 +180,13 @@ const ListChat = ({
         </div>
       </div>
 
-      <div className="contents-chats" style={{
-        height : "100%",
-        overflowY : "scroll"
-      }}>
+      <div
+        className="contents-chats"
+        style={{
+          height: "100%",
+          overflowY: "scroll",
+        }}
+      >
         {searchFriends !== null
           ? searchFriends.map((chat) => (
               <div
@@ -203,15 +220,16 @@ const ListChat = ({
               </div>
             ))
           : chats?.map((chat) => (
-                <div
-                  className="user-chat"
-                  key={chat.id}
-                  onClick={() => handleClickChat(chat)}
-                  style={{
-                    backgroundColor:
-                      chatSelected.id === chat.id ? "#e5efff" : "white",
-                  }}
-                >
+              <div
+                className="user-chat"
+                key={chat.id}
+                onClick={() => handleClickChat(chat)}
+                style={{
+                  backgroundColor:
+                    chatSelected.id === chat.id ? "#e5efff" : "white",
+                }}
+              >
+                <div style={{position : "relative"}}>
                   <img
                     src={
                       chat.image == "null"
@@ -226,84 +244,112 @@ const ListChat = ({
                       marginLeft: 20,
                     }}
                   />
-                  <div
-                    style={{ width: "40%", flexDirection: "row", marginLeft: 10 }}
-                  >
-                    <div style={{ marginTop: 10, fontSize: 18, marginLeft: 5 }}>
-                      {chat.name}
-                    </div>
-                    <div
-                      style={{
-                        fontSize: 15,
-                        marginTop: 7,
-                        color: "gray",
-                        marginLeft: 5,
-                      }}
-                    >
-                      <span>{chat.sender == userId ? "Bạn: " : ""}</span>
-                      <span>
-                        {chat.message?.length > 15
-                          ? chat.message.includes(regexUrl)
-                            ? chat.message.split("--")[1].slice(0, 10)
-                            : chat.message.slice(0, 10)
-                          : chat.message}
-                      </span>
-                      <span>{chat.message?.length > 15 ? "..." : null}</span>
-                    </div>
+                  {
+                    chat.quantity > 0 && <span style={{
+                      position : "absolute",
+                      bottom : -15,
+                      right : 0,
+                      padding : "1px 7px",
+                      borderRadius : "50%",
+                      color : "white",
+                      background : "red",
+                      fontSize : "13px"
+                    }}>{chat.quantity}</span>
+                  }
+                </div>
+                <div
+                  style={{ width: "40%", flexDirection: "row", marginLeft: 10 }}
+                >
+                  <div style={{ marginTop: 10, fontSize: 18, marginLeft: 5 }}>
+                    {chat.name}
                   </div>
                   <div
                     style={{
-                      width: "35%",
-                      textAlign: "end",
-                      display: "flex",
-                      flexDirection: "column",
-                      position: "relative",
+                      fontSize: 15,
+                      marginTop: 7,
+                      color: chat.quantity > 0 ? "black" : "gray",
+                      marginLeft: 5,
+                      fontWeight: chat.quantity > 0 && "bold",
                     }}
                   >
-                    <div style={{padding:5}}><i className="fa-solid fa-ellipsis icon" onClick={()=>handleClickUtils(chat.id)}></i></div>
+                    <span>{chat.sender == userId ? "Bạn: " : ""}</span>
                     <span>
-                      {moment
-                        .duration(
-                          moment().diff(
-                            moment(chat.dateTimeSend)
-                              .utcOffset(0)
-                              .format("YYYY-MM-DD HH:mm:ss")
-                          )
-                        )
-                        .humanize(true)}
+                      {chat.message?.length > 15
+                        ? chat.message.includes(regexUrl)
+                          ? chat.message.split("--")[1].slice(0, 10)
+                          : chat.message.slice(0, 10)
+                        : chat.message}
                     </span>
-                    {
-                      idChat===chat.id && isClickUtils && 
-                      <div style={{width:"150px", 
-                              display: "flex",
-                              flexDirection: "column",
-                              alignItems: "flex-start",
-                              justifyContent: "space-around",
-                              //padding: "5px 0 5px 10px",
-                              backgroundColor:"white",
-                              position:"absolute",
-                              zIndex:1,
-                              borderRadius: "2%",
-                              boxShadow: "0 0 5px #b4a7a7",
-                              left: -14,
-                              top:25}}>
-                              <div className="utils">Ghim hội thoại</div>
-                              <div className="utils" style={{color:"red"}} onClick={()=> setVisibleDel(true)}>Xóa hội thoại</div>
-                              <FormDeleteChat 
-                                setVisible={setVisibleDel} 
-                                visible={visibleDel}
-                                setIsClickUtils={setIsClickUtils}
-                                userId={userId} 
-                                objectId={chat}
-                                urlBackend={urlBackend} 
-                                setRerender={setRerender}
-                                setDeleteChat={setDeleteChat}
-                              />
-                              <div className="utils">Thêm vào nhóm</div>
-                      </div>
-                    }
+                    <span>{chat.message?.length > 15 ? "..." : null}</span>
                   </div>
                 </div>
+                <div
+                  style={{
+                    width: "35%",
+                    textAlign: "end",
+                    display: "flex",
+                    flexDirection: "column",
+                    position: "relative",
+                  }}
+                >
+                  <div style={{ padding: 5 }}>
+                    <i
+                      className="fa-solid fa-ellipsis icon"
+                      onClick={() => handleClickUtils(chat.id)}
+                    ></i>
+                  </div>
+                  <span>
+                    {moment
+                      .duration(
+                        moment().diff(
+                          moment(chat.dateTimeSend)
+                            .utcOffset(0)
+                            .format("YYYY-MM-DD HH:mm:ss")
+                        )
+                      )
+                      .humanize(true)}
+                  </span>
+                  {idChat === chat.id && isClickUtils && (
+                    <div
+                      style={{
+                        width: "150px",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "flex-start",
+                        justifyContent: "space-around",
+                        //padding: "5px 0 5px 10px",
+                        backgroundColor: "white",
+                        position: "absolute",
+                        zIndex: 1,
+                        borderRadius: "2%",
+                        boxShadow: "0 0 5px #b4a7a7",
+                        left: -14,
+                        top: 25,
+                      }}
+                    >
+                      <div className="utils">Ghim hội thoại</div>
+                      <div
+                        className="utils"
+                        style={{ color: "red" }}
+                        onClick={() => setVisibleDel(true)}
+                      >
+                        Xóa hội thoại
+                      </div>
+                      <FormDeleteChat
+                        setVisible={setVisibleDel}
+                        visible={visibleDel}
+                        setIsClickUtils={setIsClickUtils}
+                        userId={userId}
+                        objectId={chat}
+                        urlBackend={urlBackend}
+                        setRerender={setRerender}
+                        setDeleteChat={setDeleteChat}
+                      />
+                      <div className="utils">Thêm vào nhóm</div>
+                    </div>
+                  )}
+                </div>
+              </div>
             ))}
       </div>
     </div>
